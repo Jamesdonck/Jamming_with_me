af9b68886702692ea78f4c2264e8822028eb473c
Sergio Cerqueira <jovis.sergio@gmail.com>
2021-11-23 14:02:16 +0000

    added new to booking controller

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,17 @@
 class BookingsController < ApplicationController
+  def new
+    @instruments = Jam.all
+    @user = current_user
+    @booking = Booking.new
+  end
+
+  def create
+
+  end
+
+  private
+
+  def booking_params
+    params.require(:bookmark).permit(:instrument, :user_id, :jam_id)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  resources :jams
+  resources :jams do
+    resources :bookings
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :jams, only: %i[index show new destroy]
 end
